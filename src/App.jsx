3a88cba4521ff79a349846cf6941806548872cfb
gee452 <goitsemangmarokoane@gmail.com>
2026-08-16 00:18:05 +0200

    Fixed sign in routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
+import SignIn from "./pages/SignIn"
 import Landing from "./pages/Landing"
 import AskGogo from "./pages/AskGogo"
 import Explore from "./pages/Explore"
@@ -18,6 +19,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/askgogo" element={<AskGogo />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/contribute" element={<Contribute />} />
